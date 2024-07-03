048f0f6d1edbec783a6e7c9c93a6e6380e3a093a
Ochis27 <ochispaulcatalin27@gmail.com>
2024-07-04 00:06:46 +0300

    section 8

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -1,11 +1,18 @@
 import MainNav from '@/components/MainNav.vue';
 import { render, screen } from '@testing-library/vue';
-import { describe, expect } from 'vitest';
+import { describe, expect, it } from 'vitest';
 
 describe("MainNav", () => {
   it("displays company name", () => {
     render(MainNav);
     const companyName = screen.getByText("Bobo Careers");
     expect(companyName).toBeInTheDocument();
+  });
+
+  it("displays menu items for navigation", () => {
+    render(MainNav);
+    const navigationMenuItems = screen.getAllByRole("listitem");
+    const navigationMenuTexts = navigationMenuItems.map(item => item.textContent);
+    expect(navigationMenuTexts).toEqual(["Teams", "Locations", "Life at Bobo Corp", "How we hire", "Students", "Jobs"]);
   })
 })
